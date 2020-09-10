feb0479b24321cbd89fb2b9dcbb53542211fecc2
KD526 <62285417+KD526@users.noreply.github.com>
2020-09-10 16:51:21 +0300

    Delete sw_test.js

--- sw_test.js
+++
@@ -1,9 +0,0 @@
-//register service worker
-
-if ('serviceWorker' in navigator) {
-        navigator.serviceWorker
-        .register('/sw.js')
-        .then((reg) => console.log('Service Worker: Registered'))
-        .catch((err) => console.log('Service Worker: Not registered'));
-
-}
